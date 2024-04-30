ad0de7b8539f68299c1d257e302ba5bfec36b2f2
Valerii_Panov <val250640@mail.ru>
2024-04-30 15:16:23 +1000

    hw03 start

---
+++ Homework03/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const ACCESS_KEY = '9J7E2eUNDZoBxV94vNQ68v4LJOMKghzu0MWzHUEpLSI';
+
+const containerEl = document.querySelector('.container');
+
+
+// const url = `https://api.unsplash.com/photos?page=${page}&client_id=${ACCESS_KEY}`;
